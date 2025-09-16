20d803cc3f0338f9424e30051d57794301b96e72
theddy9098 <teddyfso15@gmail.com>
2025-09-15 22:00:52 -0500

    Implementacion de Arimetica/geometrica con ALlMATCH

--- Actividad_2/src/main/java/Librerias/Main/Main.java
+++ Actividad_2/src/main/java/Librerias/Main/Main.java
@@ -16,12 +16,33 @@ public class Main {
         else if (descendente) return "Descendente";
         else return "Desordenado";
     }
+
+    public static boolean esAritmetica(int[] arr) {
+        if (arr.length < 2) return false;
+        return IntStream.range(1, arr.length - 1)
+                .allMatch(i -> arr[i + 1] - arr[i] == arr[1] - arr[0]);
+    }
+
+
+    public static boolean esGeometrica(int[] arr) {
+        if (arr.length < 2 || arr[0] == 0) return false;
+        return IntStream.range(1, arr.length - 1)
+                .allMatch(i -> arr[i] != 0 && (double) arr[i + 1] / arr[i] == (double) arr[1] / arr[0]);
+    }
+
     public static void main(String[] args) {
 
         int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9};
         for( int i:arr)
             System.out.println(i);
         System.out.println(detectarOrden(arr));
+
+
+        for( int i:arr)
+            System.out.println(i);
+        System.out.println( esAritmetica(arr));
+        System.out.println( esGeometrica(arr));
+
 
 
 
