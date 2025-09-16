42048bcc4f6d34751fe1aa427912f137bef23a25
theddy9098 <teddyfso15@gmail.com>
2025-09-15 22:30:24 -0500

    Implementacion el metodo .Every

--- Libreria.ts
+++ Libreria.ts
@@ -7,7 +7,20 @@ function detectarOrden(arr: number[]): string {
     else if (descendente) return "Descendente";
     else return "Desordenado";
 }
+function esAritmetica(arr: number[]): boolean {
+    if (arr.length < 2) return false;
+    return arr.every((val, i, a) =>
+        i < 2 || a[i] - a[i - 1] === a[1] - a[0]
+    );
+}
 
+
+function esGeometrica(arr: number[]): boolean {
+    if (arr.length < 2 || arr[0] === 0) return false;
+    return arr.every((val, i, a) =>
+        i < 2 || a[i] / a[i - 1] === a[1] / a[0]
+    );
+}
 
 
 
@@ -15,3 +28,7 @@ function detectarOrden(arr: number[]): string {
 const Arreglo:number[] = [2, 4, 6, 8, 10];
 console.log(Arreglo);
 console.log(detectarOrden(Arreglo));
+
+console.log(Arreglo);
+console.log(esAritmetica(Arreglo));
+console.log(esGeometrica(Arreglo));
